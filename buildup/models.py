8ea9132291bb4e92c836788a0696be89b0a76416
tankorsmash <tankorsmash@gmail.com>
2016-08-07 12:16:21 -0400

    pretty print coins

--- buildup/models.py
+++ buildup/models.py
@@ -4,6 +4,14 @@ import json
 
 from django.db import models
 
+def _pretty(input):
+    result = []
+    for i, el in enumerate(input[::-1]):
+            if i%3 == 0:
+                result.append(",")
+            result.append(el)
+    return "".join(result[::-1][:-1])
+
 class Player(models.Model):
     username = models.fields.CharField(max_length=16)
     coins = models.fields.BigIntegerField(default=0)
@@ -16,3 +24,7 @@ class Player(models.Model):
     def total_building_levels(self):
         buildings = json.loads(self.building_json)
         return sum(map(lambda b: b['building_level'], buildings.values()))
+
+    @property
+    def pretty_coins(self):
+        return _pretty(str(self.coins))
