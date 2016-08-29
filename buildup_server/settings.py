1a015948ca9c15cb7f713cf0023c2bf223c69644
tankorsmash <tankorsmash@gmail.com>
2016-08-28 22:04:11 -0400

    use static bootstrap v4.a.3

--- buildup_server/settings.py
+++ buildup_server/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     'django_extensions',
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 #
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #STATIC_ROOT = '/home/tankorsmash/webapps/buildup_static/'
 
 try:
