97710e3539ee1f8ff834361f480e2b4805af004e
tankorsmash <tankorsmash@gmail.com>
2017-03-14 23:42:44 -0400

    create data generator functions

--- leagion/utils.py
+++ leagion/utils.py
@@ -1,1 +1,43 @@
+import faker
 import humanize
+
+from leagion.models import User, Team, League, Match, Roster
+
+def generate_users(count, team=None):
+    f = faker.Faker()
+
+    for i in xrange(count):
+        print "generating user", i+1, "of", count
+        user = User.objects.create(
+            username=f.user_name(),
+            email=f.email(),
+            first_name=f.first_name(),
+            last_name=f.last_name(),
+            password=f.password()
+        )
+
+        if team:
+            team.players.add(user)
+
+def generate_league(name=None, teams_count=5, players_in_team_count=15):
+    print "generating league"
+    if name is None:
+        f = faker.Faker()
+        name = f.company()+" League"
+    league = League.objects.create(name=name)
+
+    print "generating teams for league"
+    generate_teams(league, teams_count, players_in_team_count)
+
+    return league
+
+
+def generate_teams(league, team_count, players_count):
+    f = faker.Faker()
+    for i in xrange(team_count):
+        team = Team.objects.create(
+            name=f.street_name(),
+            league=league
+        )
+        print "generating players for team", i+1, "of", team_count
+        generate_users(players_count, team)
--- leagion/views.py
+++ leagion/views.py
@@ -12,11 +12,33 @@ from django.views.generic import TemplateView
 
 from django.http import HttpResponse, JsonResponse
 
-from leagion.models import User, Team, League
+from leagion.models import User, Team, League, Match, Roster
 
 # views
 class Index(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self):
-        return {}
+        context = {"leagues": []}
+        for league in League.objects.all():
+            league_ctx = {
+                "name": league.name,
+                "teams": []
+            }
+            for team in league.teams.all():
+                team_ctx = {
+                    "name": team.name,
+                    "players": []
+                }
+                for player in team.players.all():
+                    player_ctx = {
+                        "full_name": "{} {}".format(player.first_name, player.last_name)
+                    }
+                    team_ctx["players"].append(player_ctx)
+
+                league_ctx["teams"].append(team_ctx)
+
+            context["leagues"].append(league_ctx)
+
+
+        return context
