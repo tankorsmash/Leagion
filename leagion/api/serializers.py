16d63f72c4041edc6a53bd2fde1c2c29ffa38524
tankorsmash <tankorsmash@gmail.com>
2017-03-19 20:42:18 -0400

    add match serializer

--- leagion/api/serializers.py
+++ leagion/api/serializers.py
@@ -2,8 +2,20 @@ from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
+from leagion.models import Match, Roster, Team, League
+
 # Serializers define the API representation.
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name',)
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = (
+            'id', 'match_datetime', 'location', 'league', 'duration_seconds',
+            'away_team', 'away_points','away_team', 'away_points', 'status',
+            'postponed_to', 'postponed_from', 'pretty_name',
+        )
