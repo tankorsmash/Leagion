41144d0d955e313fd886940cdf3b81dcb9bb0da4
tankorsmash <tankorsmash@gmail.com>
2017-03-19 21:22:37 -0400

    whitespace

--- leagion/api/views.py
+++ leagion/api/views.py
@@ -6,9 +6,11 @@ from rest_framework.permissions import IsAdminUser
 from leagion.api import serializers
 from leagion.models import Match, Roster, Team, League
 
+
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
 
 class UserDetail(generics.RetrieveUpdateAPIView):
     lookup_url_kwarg = "player_id"
@@ -16,9 +18,11 @@ class UserDetail(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
+
 class MatchList(generics.ListCreateAPIView):
     queryset = Match.objects.all()
     serializer_class = serializers.MatchSerializer
+
 
 class MatchDetail(generics.RetrieveUpdateAPIView):
     lookup_url_kwarg = "match_id"
