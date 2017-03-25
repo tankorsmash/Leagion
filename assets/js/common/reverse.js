0ce72d9ce5a0d598d4f4e54f78293b276a7d40c0
Calvin Collins <calvinkcollins@gmail.com>
2017-03-24 21:37:13 -0400

    added knox for authentication and reverse for javascript reversing of urls

---
+++ assets/js/common/reverse.js
@@ -0,0 +1,32 @@
+
+//Generated File
+//Do not modify by hand, it will get overwritten
+//see leagion reverse_js for what you want
+var viewnames = {
+    "api-match-detail": "/api/match/<match_id>/",
+    "api-match-list": "/api/match/",
+    "api-player-detail": "/api/player/<player_id>/",
+    "api-player-list": "/api/player/",
+    "knox_login": "/login/",
+    "knox_logout": "/logout/",
+    "knox_logoutall": "/logoutall/"
+};
+
+function reverse(urlname, kwargs) {
+    var url = viewnames[urlname];
+    kwargs = kwargs || {};
+
+    if (url === undefined) {
+        throw 'reverse failed. Incorrect urlname: ' + urlname;
+    }
+
+    for(var kwarg in kwargs) {
+        var value = kwargs[kwarg];
+        url = url.replace('<' + kwarg + '>', value);
+    }
+    if (url.includes('<')) {
+        throw 'reverse failed. Missing kwargs. urlname: ' + urlname + '. kwargs: ' + JSON.stringify(kwargs) + '. url: ' + url;
+    }
+    return url;
+}
+window.reverse = reverse;
--- leagion/api/views.py
+++ leagion/api/views.py
@@ -6,24 +6,26 @@ from rest_framework.permissions import IsAdminUser
 from leagion.api import serializers
 from leagion.models import Match, Roster, Team, League
 
+from leagion.utils import reverse_js
 
+@reverse_js
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
-
+@reverse_js
 class UserDetail(generics.RetrieveUpdateAPIView):
     lookup_url_kwarg = "player_id"
 
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
-
+@reverse_js
 class MatchList(generics.ListCreateAPIView):
     queryset = Match.objects.all()
     serializer_class = serializers.MatchSerializer
 
-
+@reverse_js
 class MatchDetail(generics.RetrieveUpdateAPIView):
     lookup_url_kwarg = "match_id"
 
