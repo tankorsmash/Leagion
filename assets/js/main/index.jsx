afe9599ff904a18863a541b3d543f4d7591e7227
Calvin Collins <calvinkcollins@gmail.com>
2017-03-27 22:19:49 -0400

    made registration jsx files more succinct

--- assets/js/main/index.jsx
+++ assets/js/main/index.jsx
@@ -4,10 +4,8 @@ import {Container, Row, Col} from 'reactstrap';
 import {Navbar} from 'components/nav';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import App from 'main/app.jsx';
-import Login from 'main/registration/login';
-import Register from 'main/registration/register';
-import auth from 'main/registration/auth';
-import {root, app} from 'common/urls';
+import {RegisterForm, LoginForm, auth} from 'main/registration';
+import urls from 'common/urls';
 
 const PrivateRoute = ({ component, ...rest }) => (
     <Route {...rest} render={props => (
@@ -15,7 +13,7 @@ const PrivateRoute = ({ component, ...rest }) => (
             React.createElement(component, props)
         ) : (
             <Redirect to={{
-                pathname: `${root}/login`,
+                pathname: `${urls.root}/login`,
                 state: { from: props.location }
             }}/>
         )
@@ -29,9 +27,9 @@ ReactDOM.render(
             <Container>
                 <Row>
                     <Col>
-                        <Route path={`${root}/login/`} component={Login} />
-                        <Route path={`${root}/register/`} component={Register} />
-                        <PrivateRoute path={`${app}/`} component={App}/>
+                        <Route path={`${urls.root}/login/`} component={LoginForm} />
+                        <Route path={`${urls.root}/register/`} component={RegisterForm} />
+                        <PrivateRoute path={`${urls.app}/`} component={App}/>
                     </Col>
                 </Row>
             </Container>
