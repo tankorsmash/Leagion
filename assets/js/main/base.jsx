e24da0bc1911bece7a5c67bef348f76902814a55
Calvin Collins <calvinkcollins@gmail.com>
2017-04-01 19:00:33 -0400

    fix hot reloader

---
+++ assets/js/main/base.jsx
@@ -0,0 +1,83 @@
+import ajax from 'common/ajax';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {Container, Row, Col} from 'reactstrap';
+import {Navbar} from 'components/nav';
+import {RegisterForm, LoginForm, auth} from 'main/registration';
+import urls from 'common/urls';
+import App from 'main/app';
+
+const PrivateRoute = ({ component, ...rest }) => (
+    <Route {...rest} render={props => (
+        auth.loggedIn() ? (
+            React.createElement(component, props)
+        ) : (
+            <Redirect to={{
+                pathname: urls.login,
+                state: { from: props.location }
+            }}/>
+        )
+    )}/>
+)
+
+class Public extends React.Component {
+    render() {
+        return (
+            <div>Your at the public page!</div>
+        );
+    }
+}
+
+const FourOhFour = (props) => {
+    return (
+        <div>404</div>
+    );
+}
+
+const Main = ({match}) => {
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <Row>
+                    <Col>
+                        <main>
+                            <Switch>
+                                <Route exact path={urls.root} component={Public} />
+                                <Route path={urls.login} component={LoginForm} />
+                                <Route path={urls.register} component={RegisterForm} />
+                                <PrivateRoute path={urls.app.index} component={App}/>
+                                <Route component={FourOhFour} />
+                            </Switch>
+                        </main>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+class Base extends React.Component {
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            //isAuthenticated: false,
+        };
+    }
+
+    logout() {
+        //this.setState({isAuthenticated: false});
+    }
+
+    render() {
+
+        return (
+            <Router>
+                <Route path={`${urls.root}/`} component={Main} />
+            </Router>
+        );
+    }
+}
+
+module.exports = Base;
+
--- assets/js/main/index.jsx
+++ assets/js/main/index.jsx
@@ -1,85 +1,18 @@
 require("expose-loader?reverse!common/reverse");
 
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
-import {Container, Row, Col} from 'reactstrap';
-import {Navbar} from 'components/nav';
-import App from 'main/app.jsx';
-import {RegisterForm, LoginForm, auth} from 'main/registration';
-import urls from 'common/urls';
+import { AppContainer  } from 'react-hot-loader';
+import Base from 'main/base';
 
-const PrivateRoute = ({ component, ...rest }) => (
-    <Route {...rest} render={props => (
-        auth.loggedIn() ? (
-            React.createElement(component, props)
-        ) : (
-            <Redirect to={{
-                pathname: urls.login,
-                state: { from: props.location }
-            }}/>
-        )
-    )}/>
-)
-
-class Public extends React.Component {
-    render() {
-        return (
-            <div>Your at the public page!</div>
-        );
-    }
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.getElementById('root')
+	)
 }
 
-const FourOhFour = (props) => {
-    return (
-        <div>404</div>
-    );
-}
+render(Base);
 
-const Main = ({match}) => {
-    return (
-        <div>
-            <Navbar />
-            <Container>
-                <Row>
-                    <Col>
-                        <main>
-                            <Switch>
-                                <Route exact path={urls.root} component={Public} />
-                                <Route path={urls.login} component={LoginForm} />
-                                <Route path={urls.register} component={RegisterForm} />
-                                <PrivateRoute path={urls.app.index} component={App}/>
-                                <Route component={FourOhFour} />
-                            </Switch>
-                        </main>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-}
-
-class Index extends React.Component {
-    constructor(props) {
-
-        super(props);
-        this.state = {
-            //isAuthenticated: false,
-        };
-    }
-
-    logout() {
-        //this.setState({isAuthenticated: false});
-    }
-
-    render() {
-
-        return (
-            <Router>
-                <Route path={`${urls.root}/`} component={Main} />
-            </Router>
-        );
-    }
-}
-
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+if (module.hot) module.hot.accept('main/base', () => render(Base));
