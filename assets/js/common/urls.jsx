71905c57352491ee5005b48c05322defed31f67e
Calvin Collins <calvinkcollins@gmail.com>
2017-04-01 13:32:23 -0400

    routes working properly

--- assets/js/common/urls.jsx
+++
@@ -1,7 +0,0 @@
-let root = '/main';
-let app = `${root}/app`;
-
-module.exports = {
-	root: root,
-	app: app
-}
--- assets/js/main/index.jsx
+++ assets/js/main/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Container, Row, Col} from 'reactstrap';
 import {Navbar} from 'components/nav';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, IndexRoute, Redirect} from 'react-router-dom';
 import App from 'main/app.jsx';
 import {RegisterForm, LoginForm, auth} from 'main/registration';
-import urls from 'common/urls';
+import {root} from 'common/urls';
 
 const PrivateRoute = ({ component, ...rest }) => (
     <Route {...rest} render={props => (
@@ -13,27 +13,69 @@ const PrivateRoute = ({ component, ...rest }) => (
             React.createElement(component, props)
         ) : (
             <Redirect to={{
-                pathname: `${urls.root}/login`,
+                pathname: `${root}/login`,
                 state: { from: props.location }
             }}/>
         )
     )}/>
 )
 
-ReactDOM.render(
-    <Router>
+class Public extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>Your at the public page!</div>
+        );
+    }
+}
+
+
+const Main = ({match}) => {
+    return (
         <div>
-            <Navbar/>
+            <Navbar />
             <Container>
                 <Row>
                     <Col>
-                        <Route path={`${urls.root}/login/`} component={LoginForm} />
-                        <Route path={`${urls.root}/register/`} component={RegisterForm} />
-                        <PrivateRoute path={`${urls.app}/`} component={App}/>
+                        <main>
+                            <Route component={Public} />
+                            <Route path={`${match.url}/login`} component={LoginForm} />
+                            <Route path={`${match.url}/register`} component={RegisterForm} />
+                            <PrivateRoute path={`${match.url}/app/`} component={App}/>
+                        </main>
                     </Col>
                 </Row>
             </Container>
         </div>
-    </Router>,
-    document.getElementById('root')    
-)
+    );
+}
+
+class Index extends React.Component {
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            isAuthenticated: false,
+        };
+    }
+
+    logout() {
+        this.setState({isAuthenticated: false});
+    }
+
+    render() {
+
+        return (
+            <Router>
+                <Route path={`${root}/`} component={Main} />
+            </Router>
+        );
+    }
+}
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
