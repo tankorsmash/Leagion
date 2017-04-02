b68995ac03a50e15bc8f9cdaf15a8da3f7d341f9
Calvin Collins <calvinkcollins@gmail.com>
2017-04-02 09:17:22 -0400

    modularizing the nav bar

--- assets/js/components/buttons.jsx
+++ assets/js/components/buttons.jsx
@@ -3,25 +3,17 @@ import urls from 'common/urls';
 import auth from 'main/auth';
 
 const LogoutButton = ({...rest}) => {
-    if (auth.loggedIn()) {
-        return (
-            <Link {...rest} to={urls.login} onClick={auth.logout}>
-                Logout
-            </Link>
-        );
-    } else {
-        return null;
-    }
+	return (
+		<Link {...rest} to={urls.login} onClick={auth.logout}>
+			Logout
+		</Link>
+	);
 }
 
 const LoginButton = ({...rest}) => {
-    if (!auth.loggedIn()) {
-        return (
-            <Link {...rest} to={urls.login}>Login</Link>
-        );
-    } else {
-        return null;
-    }
+	return (
+		<Link {...rest} to={urls.login}>Login</Link>
+	);
 }
 
 module.exports = {
