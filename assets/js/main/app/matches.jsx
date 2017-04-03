2faccf72e2e3656da85c599bc4c9fe25a0600928
tankorsmash <tankorsmash@gmail.com>
2017-04-03 00:19:16 -0400

    ajax in match data from the api

--- assets/js/main/app/matches.jsx
+++ assets/js/main/app/matches.jsx
@@ -1,60 +1,61 @@
+import ajax from 'common/ajax';
+
 function getRandomInt(min, max) {
     //min and max inclusive
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 class Match extends React.Component {
-   render() {
-      return (
-         <div className="mt-3">
-            <div>
-                Match name: { this.props.name }
+    render() {
+        let match = this.props.match;
+        return (
+            <div className="mt-3">
+                <h2>
+                    Match name: { match.pretty_name }
+                </h2>
+                <div>
+                    Match location: { match.location }
+                </div>
+                <div>
+                    home points: { match.home_points }
+                </div>
+                <div>
+                    away points: { match.away_points }
+                </div>
+                <div>
+                   duration: { match.duration_seconds }
+                </div>
             </div>
-            <div>
-                Match location: { this.props.location }
-            </div>
-            <div>
-                home score: { this.props.home_score }
-            </div>
-            <div>
-                away score: { this.props.away_score }
-            </div>
-         </div>
-      );
-   }
+        );
+    }
 }
 
 class Matches extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            matches: []
-        };
-
-        [...Array(10)].fill().map(()=> { //for _ in range(10):
-            let match = {
-                name: "--MATCH_NAME",
-                location: "--MATCH_LOCATION",
-                home_score: getRandomInt(0, 25),
-                away_score: getRandomInt(4, 20),
-            }
-            this.state.matches.push(match);
-        });
+        this.state = { matches: [] };
     }
+
     render() {
         return (
             <div>
                 this is the list of matches
                 { this.state.matches.map((match)=>{
                     return <Match
-                        name= {match.name}
-                        location= {match.location}
-                        home_score= {match.home_score}
-                        away_score= {match.away_score}
+                        match={match}
+                        key={match.id}
                     />
                 }) }
             </div>
         );
+    }
+
+    componentDidMount() {
+      ajax({
+         url: reverse('api-match-list'),
+      }).then(data => {
+          this.setState({matches: data});
+      });
     }
 }
 
