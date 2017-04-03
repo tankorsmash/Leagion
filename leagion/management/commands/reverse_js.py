3006a0b3a964889c1929132ac01f4732acf25d66
tankorsmash <tankorsmash@gmail.com>
2017-04-03 00:02:37 -0400

    tidy up reverse_js command a bit

--- leagion/management/commands/reverse_js.py
+++ leagion/management/commands/reverse_js.py
@@ -1,10 +1,11 @@
-import json
 import os
+import json
 
 from django.conf import settings
 from django.contrib.admindocs.views import simplify_regex
 from django.core.management.base import BaseCommand
 from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
+
 from django.template import Context, Template
 
 from leagion.utils import viewnames_exposed_js
@@ -13,44 +14,49 @@ from leagion.utils import viewnames_exposed_js
 class Command(BaseCommand):
     help = "Create a js version of reverse"
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         from leagion_server import urls as urlconf
-        template = Template(viewnames_template)
+        print("starting to generate reverse for django views")
         view_functions = self.extract_views_from_urlpatterns(urlconf.urlpatterns)
-        views = {  # dict(view_functions) would work as well but I find this more readable
-            url_name: url
-            for url_name, url in view_functions
-            }
+        views = {
+            url_name: url for url_name, url in view_functions
+        }
+
         reverse_js_path = os.path.join(
             settings.ROOT_PATH,
             'assets/js/common/reverse.jsx'
-            )
+        )
 
         with open(reverse_js_path, 'wt') as reverse_js_file:
+            template = Template(viewnames_template)
             reverse_js_file.write(template.render(Context({
                 'viewnames_json': json.dumps(views, sort_keys=True, indent=4, separators=(',', ': '))
-                })))
+            })))
+
+        print("found {} views".format(len(views)))
 
     def extract_views_from_urlpatterns(self, urlpatterns, base='', namespace=None):
         views = []
-        for p in urlpatterns:
-            if isinstance(p, RegexURLPattern):
-                view = p.callback
+        for pattern in urlpatterns:
+            if isinstance(pattern, RegexURLPattern):
+                view = pattern.callback
                 view_class = getattr(view, 'view_class', None)
                 expose_js_viewname = view in viewnames_exposed_js or view_class in viewnames_exposed_js
-                name = '{0}:{1}'.format(namespace, p.name) if namespace else p.name
+                name = '{0}:{1}'.format(namespace, pattern.name) if namespace else pattern.name
 
                 if expose_js_viewname and name:
-                    views.append((name, simplify_regex(base + p.regex.pattern)))
-            elif isinstance(p, RegexURLResolver):
-                patterns = p.url_patterns
-                if namespace and p.namespace:
-                    _namespace = '{0}:{1}'.format(namespace, p.namespace)
+                    views.append((name, simplify_regex(base + pattern.regex.pattern)))
+
+            elif isinstance(pattern, RegexURLResolver):
+                patterns = pattern.url_patterns
+                if namespace and pattern.namespace:
+                    _namespace = '{0}:{1}'.format(namespace, pattern.namespace)
                 else:
-                    _namespace = (p.namespace or namespace)
-                views.extend(self.extract_views_from_urlpatterns(patterns, base + p.regex.pattern, namespace=_namespace))
+                    _namespace = (pattern.namespace or namespace)
+                views.extend(self.extract_views_from_urlpatterns(patterns, base + pattern.regex.pattern, namespace=_namespace))
             else:
-                raise TypeError("%s does not appear to be a urlpattern object" % p)
+                raise TypeError("%s does not appear to be a urlpattern object" % pattern)
+
         return views
 
 
