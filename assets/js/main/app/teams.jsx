86a2f8d5950bd5106f3a1c5d2d82b0e28d369e5f
tankorsmash <tankorsmash@gmail.com>
2017-04-03 01:20:06 -0400

    add base for players in teams list

--- assets/js/main/app/teams.jsx
+++ assets/js/main/app/teams.jsx
@@ -1,13 +1,54 @@
 import ajax from 'common/ajax';
 
+import {SimplePlayer} from 'main/app/players';
+
+class Team extends React.Component {
+    render() {
+        let team = this.props.team;
+        return (
+            <div>
+                Team name: { team.name }
+
+                <div className="pl-sm-3">
+                    { team.players.map(player => {
+                        return <SimplePlayer
+                            player={player}
+                            key={player.id}
+                        />
+                    }) }
+                </div>
+            </div>
+        );
+    }
+}
+
+
 class Teams extends React.Component {
-   render() {
-      return (
-         <div>
-            this is Teams
-         </div>
-      );
-   }
+    constructor(props){
+        super(props);
+        this.state = { teams: [] };
+    }
+
+    componentDidMount() {
+        ajax({
+            url: reverse('api-team-list'),
+        }).then(data => {
+            this.setState({teams: data});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                { this.state.teams.map((team)=>{
+                    return <Team
+                        team={team}
+                        key={team.id}
+                    />
+                })}
+            </div>
+        );
+    }
 }
 
 module.exports = Teams;
