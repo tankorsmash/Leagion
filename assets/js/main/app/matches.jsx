35b1d9c300b25eafee6cdf8c3c5b615ac1495f2a
tankorsmash <tankorsmash@gmail.com>
2017-04-02 20:04:49 -0400

    more placeholder match work

--- assets/js/main/app/matches.jsx
+++ assets/js/main/app/matches.jsx
@@ -1,11 +1,33 @@
-class Matches extends React.Component {
-   render() {
+class Match extends React.Component {
+   render(props) {
       return (
          <div>
-            this is Matches
+            this is a match
+            <div>
+                Match name: NAME
+            </div>
+            <div>
+                Match location: LOCATION
+            </div>
          </div>
       );
    }
+}
+
+class Matches extends React.Component {
+    render() {
+        let matches = [];
+        for (var i=0; i < 10; i++){
+            matches.push(<Match key={i}/>);
+        };
+
+        return (
+            <div>
+                this is the list of matches
+                {matches}
+            </div>
+        );
+    }
 }
 
 module.exports = Matches;
