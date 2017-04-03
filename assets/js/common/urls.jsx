886c2e40e5afe3d7d17932f3f09731459dbc3b2f
Calvin Collins <calvinkcollins@gmail.com>
2017-04-03 08:36:22 -0400

    fix 404 page and removed tailing and exact matches on teams and matches. We don't need to add a trailing slash as react router handles it without one

--- assets/js/common/urls.jsx
+++ assets/js/common/urls.jsx
@@ -3,8 +3,6 @@ let appUrl = `${rootUrl}/app`;
 let teamsUrl = `${appUrl}/teams`;
 let matchesUrl = `${appUrl}/matches`;
 
-let endOf = (path) => { return `${path}/`; };
-
 module.exports = {
     root: `${rootUrl}`,
 
@@ -14,14 +12,11 @@ module.exports = {
 
 	//app
 	app: {
-        tail: appUrl,
-        index: endOf(appUrl),
+        index: appUrl,
 		teams: {
-			tail: endOf(teamsUrl),
 			index: teamsUrl,
 		},
 		matches: {
-			tail: endOf(matchesUrl),
 			index: matchesUrl,
 		},
 		admin: {
---
+++ assets/js/components/error-pages.jsx
@@ -0,0 +1,7 @@
+const FourOhFour = (props) => {
+    return <h1>404</h1> ;
+}
+
+module.exports = {
+	FourOhFour: FourOhFour,
+}
