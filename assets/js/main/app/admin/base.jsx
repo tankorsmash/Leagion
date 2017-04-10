62418110dc4ddf6fbdd32f3597248e05efc1c674
tankorsmash <tankorsmash@gmail.com>
2017-04-09 21:55:01 -0400

    use bg-faded for app, then white background for content

--- assets/js/main/app/admin/base.jsx
+++ assets/js/main/app/admin/base.jsx
@@ -17,10 +17,13 @@ import {FourOhFour} from 'components/error-pages';
 
 class Admin extends React.Component {
     render() {
+        let style = {
+            backgroundColor: "white",
+        };
         return (
-            <div>
+            <div className="bg-faded">
                 <AdminNavbar {...this.props} />
-                <Container>
+                <Container style={style}>
                     <Row>
                         <Col>
                             <Switch>
