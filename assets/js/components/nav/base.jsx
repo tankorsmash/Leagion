76142423d9057ea64bf462788e08bd07a59a295a
tankorsmash <tankorsmash@gmail.com>
2017-04-09 21:44:18 -0400

    change base navbar to have box shadow instead of bg-faded, thisway we can use faded within the container

--- assets/js/components/nav/base.jsx
+++ assets/js/components/nav/base.jsx
@@ -28,9 +28,13 @@ class Navbar extends React.Component {
         });
     }
     render() {
+        const style = {
+            boxShadow: "0 1.5px 3px rgba(0,0,0,0.16), 0 1.5px 3px rgba(0,0,0,0.23)",
+        };
+
         return (
             <div>
-                <BSNavbar color="faded" light toggleable>
+                <BSNavbar style={style} light toggleable>
                     <NavbarBrand href={urls.root}>Leagion</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
