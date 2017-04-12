84b675b5407ef527293e78d917a38b836000a979
Calvin Collins <calvinkcollins@gmail.com>
2017-04-11 22:54:35 -0400

    component should be match not season, it makes much more sense

--- assets/js/components/app/match.jsx
+++ assets/js/components/app/match.jsx
@@ -1,6 +1,37 @@
 import {Link} from 'react-router-dom';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, Table } from 'reactstrap';
 import matchUrls from 'main/app/player/match/urls';
+
+const MatchTable = (props) => {
+    return (
+        <Table>
+            <thead>
+                <tr>
+                    <th>Home Team</th>
+                    <th></th>
+                    <th>Away Team</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.matches.map((match, i) => {
+                    return (
+                        <tr key={i}>
+                            <td>{match.home_team.name}</td>
+                            <td>vs.</td>
+                            <td>{match.away_team.name}</td>
+                            <td>{match.pretty_date}</td>
+                            <td>{match.pretty_time}</td>
+                            <td>{match.location.name}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+    );
+}
 
 const MatchList = (props) => {
 	return (
@@ -19,5 +50,6 @@ const MatchList = (props) => {
 };
 module.exports = {
 	MatchList: MatchList,
+	MatchTable: MatchTable,
 }
 
