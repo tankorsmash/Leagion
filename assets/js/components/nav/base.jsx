ce59289c060f5ff75ecec27ec5e8730937b05bab
Calvin Collins <calvinkcollins@gmail.com>
2017-04-14 10:16:18 -0400

    renaming nav bar to app bar

--- assets/js/components/nav/base.jsx
+++ assets/js/components/nav/base.jsx
@@ -2,7 +2,7 @@ import ajax from 'common/ajax';
 
 import {Link} from 'react-router-dom';
 
-import { Collapse, Navbar as BSNavbar, NavbarToggler,
+import { Collapse, Navbar, NavbarToggler,
     NavbarBrand, NavDropdown, DropdownToggle, DropdownMenu,
     DropdownItem, Nav, NavItem, NavLink
 } from 'reactstrap';
@@ -13,7 +13,7 @@ import auth from 'main/auth';
 
 import {NOT_LOADED, DO_NOTHING, STOP_PROPAGATION} from 'common/constants';
 
-class Navbar extends React.Component {
+class Appbar extends React.Component {
     constructor(props) {
         super(props);
 
@@ -28,25 +28,21 @@ class Navbar extends React.Component {
         });
     }
     render() {
-        const style = {
-            boxShadow: "0 1.5px 3px rgba(0,0,0,0.16), 0 1.5px 3px rgba(0,0,0,0.23)",
-        };
-
         return (
             <div>
-                <BSNavbar style={style} className="mb-3" light toggleable>
+                <Navbar className="mb-3" light toggleable>
                     <NavbarBrand href={urls.root}>Leagion</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         {React.createElement(this.itemComponent, this.props)}
                         {React.createElement(this.profileComponent, this.props)}
                     </Collapse>
-                </BSNavbar>
+                </Navbar>
             </div>
         );
     }
 }
 
 module.exports = {
-    Navbar: Navbar
+    Appbar: Appbar
 }
