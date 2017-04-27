e3885ec591f8dec2610bcccc2dffd0ad701b2723
tankorsmash <tankorsmash@gmail.com>
2017-04-27 02:23:38 -0400

    cleaner season name

--- assets/js/main/app/admin/components/seasons.jsx
+++ assets/js/main/app/admin/components/seasons.jsx
@@ -25,7 +25,7 @@ class Season extends React.Component {
                 <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
                 <CardBlock>
                     <CardTitle>
-                        Season name: { season.pretty_name }
+                        { season.start_date } to {season.end_date}
                     </CardTitle>
                     <CardSubtitle>Card subtitle</CardSubtitle>
                     <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
