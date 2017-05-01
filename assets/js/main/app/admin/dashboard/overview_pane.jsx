96f380ae0a557d08b478f9778edfdb5ef176444f
tankorsmash <tankorsmash@gmail.com>
2017-04-30 23:10:49 -0400

    try out propTypes in Notification card

--- assets/js/main/app/admin/dashboard/overview_pane.jsx
+++ assets/js/main/app/admin/dashboard/overview_pane.jsx
@@ -5,24 +5,26 @@ import {
 
 import FontAwesome from 'react-fontawesome';
 
+import PropTypes from 'prop-types';
+
 class NotificationCard extends React.Component {
     render() {
         return (
-            <Card inverse color="primary">
+            <Card inverse color={ this.props.color }>
                 <CardHeader>
                     <Row>
-                        <Col xs="3"> <FontAwesome name="comments" size="5x"/> </Col>
+                        <Col xs="3"> <FontAwesome name={ this.props.iconName } size="5x"/> </Col>
                         <Col xs="9" className="text-right">
-                            <div className="display-4">26</div>
-                            <div>New Comments!</div>
+                            <div className="display-4">{ this.props.count }</div>
+                            <div>{ this.props.headline }</div>
                         </Col>
                     </Row>
                 </CardHeader>
 
                 <a href="#">
                     <div className="card-footer">
-                        <span className="float-left">View Details</span>
-                        <span className="float-right"><i className="fa fa-arrow-circle-right"></i></span>
+                        <span className="float-left">{ this.props.actionLine }</span>
+                        <span className="float-right"><FontAwesome name="arrow-circle-right"/></span>
                         <div className="clearfix"></div>
                     </div>
                 </a>
@@ -31,13 +33,29 @@ class NotificationCard extends React.Component {
     }
 };
 
+NotificationCard.defaultProps = {
+    count: 99,
+    headline: "New Notifications!",
+    actionLine: "View Details",
+    iconName: "comments",
+    color: "primary",
+};
+
+NotificationCard.propTypes = {
+    count: PropTypes.number,
+    headline: PropTypes.string,
+    actionLine: PropTypes.string,
+    iconName: PropTypes.string,
+    color: PropTypes.string,
+};
+
 export class OverviewPane extends React.Component {
     render() {
         return (
             <Row className="d-flex justify-content-around">
-                <NotificationCard/>
-                <NotificationCard/>
-                <NotificationCard/>
+                <NotificationCard color="info"/>
+                <NotificationCard color="success" />
+                <NotificationCard count={12}/>
             </Row>
         );
     }
