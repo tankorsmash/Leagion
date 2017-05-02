af5f26e6d4180328df4e6738c3e641249d8b2d18
tankorsmash <tankorsmash@gmail.com>
2017-05-01 22:05:06 -0400

    remove unused components for admin dashboard

--- assets/js/main/app/admin/dashboard.jsx
+++ assets/js/main/app/admin/dashboard.jsx
@@ -40,36 +40,6 @@ class LeagueCard extends React.Component {
 };
 
 
-class ContentHeader extends React.Component {
-    render() {
-        return (
-            <div>
-                <Jumbotron>
-                    <h1 className="display-3">Dashboard</h1>
-                    <p className="lead">Overview of the league management</p>
-                    <hr className="my-2" />
-                    <p>As a league manager, you're able to view all teams and players.
-                        You'll be able to create and edit teams,
-                        assign coaches and team managers, and set up schedules.</p>
-                    <p className="lead">
-                        <Button color="primary">Learn More</Button>
-                    </p>
-                </Jumbotron>
-            </div>
-        );
-    }
-};
-
-class MainContent extends React.Component {
-    render() {
-        return (
-            <div>
-                <ContentHeader/>
-            </div>
-        );
-    }
-};
-
 class LeaguesPane extends DatasetView {
     get datasetStateAttr() {
         return "leagues";
