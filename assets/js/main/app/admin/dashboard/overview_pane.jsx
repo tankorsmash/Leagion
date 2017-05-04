c499779b51d84da0c73c4b782df0cb97500a38d5
tankorsmash <tankorsmash@gmail.com>
2017-05-04 01:22:56 -0400

    add links to team and leagues panes in the notification cards

--- assets/js/main/app/admin/dashboard/overview_pane.jsx
+++ assets/js/main/app/admin/dashboard/overview_pane.jsx
@@ -3,11 +3,15 @@ import {
     Card, CardImg, CardText, CardBlock, CardTitle, CardSubtitle,
 } from 'reactstrap';
 
+import {Link} from 'react-router-dom';
+
 import FontAwesome from 'react-fontawesome';
 
 import PropTypes from 'prop-types';
 
 import {DatasetView} from 'components/dataset_view';
+
+import adminUrls from 'main/app/admin/urls';
 
 class NotificationCard extends React.Component {
     render() {
@@ -23,13 +27,13 @@ class NotificationCard extends React.Component {
                     </Row>
                 </CardHeader>
 
-                <a href="#">
+                <Link to={this.props.actionHref}>
                     <div className="card-footer">
                         <span className="float-left">{ this.props.actionLine }</span>
                         <span className="float-right"><FontAwesome name="arrow-circle-right"/></span>
                         <div className="clearfix"></div>
                     </div>
-                </a>
+                </Link>
             </Card>
         );
     }
@@ -39,6 +43,7 @@ NotificationCard.defaultProps = {
     count: 99,
     headline: "New Notifications!",
     actionLine: "View Details",
+    actionHref: "#",
     iconName: "comments",
     color: "primary",
 };
@@ -47,6 +52,7 @@ NotificationCard.propTypes = {
     count: PropTypes.number,
     headline: PropTypes.string,
     actionLine: PropTypes.string,
+    actionHref: PropTypes.string,
     iconName: PropTypes.string,
     color: PropTypes.string,
 };
@@ -65,9 +71,9 @@ class NotificationCards extends DatasetView {
         return (
             <Row className="d-flex justify-content-around align-items-stretch">
                 <Col md="3"> <NotificationCard count={stats.player_count} headline="Players registered" iconName="users" color="info"/> </Col>
-                <Col md="3"> <NotificationCard count={stats.league_count} headline="Leagues created" iconName="users" color="warning"/> </Col>
+                <Col md="3"> <NotificationCard count={stats.league_count} actionHref={adminUrls.dashboard.leagues} headline="Leagues created" iconName="users" color="warning"/> </Col>
                 <Col md="3"> <NotificationCard count={stats.match_count} headline="Matches played" iconName="star" color="success" /> </Col>
-                <Col md="3"> <NotificationCard count={stats.team_count} headline="Teams registered" iconName="th-list" /> </Col>
+                <Col md="3"> <NotificationCard count={stats.team_count} actionHref={adminUrls.dashboard.teams} headline="Teams registered" iconName="th-list" /> </Col>
             </Row>
         );
     }
