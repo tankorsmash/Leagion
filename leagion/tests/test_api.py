7ee2280e20ee8994cb0ce5c5bcf13cce22159d43
tankorsmash <tankorsmash@gmail.com>
2017-05-05 09:05:24 -0400

    stats dashboard api test

--- leagion/tests/test_api.py
+++ leagion/tests/test_api.py
@@ -1,16 +1,22 @@
-from django.test import TestCase
+import datetime
+
+from django.test import TestCase, override_settings
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 
 from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate
 
-from leagion.models import League
+from leagion.models import League, Season, Team
 User = get_user_model()
 
 from leagion.api import views as api_views
 
+@override_settings(ROOT_URLCONF="leagion_server.urls")
 class ApiTest(APITestCase):
-    urls = "leagion_server.urls"
+    """
+    simple cases for now, but once we get permissions going, we'll use this
+    to make sure a given user can't see too much from the api
+    """
 
     def setUp(self):
         self.user = User.objects.create_superuser(
@@ -30,3 +36,39 @@ class ApiTest(APITestCase):
 
         response = api_views.leagues.LeagueList.as_view()(request)
         self.assertEquals(response.data[0]['id'], 1)
+
+    def test_stats(self):
+        league = League.objects.create(
+            name="Test League",
+        )
+
+        season = Season.objects.create(
+            league=league,
+            start_date = datetime.datetime.now(),
+            end_date = datetime.datetime.now(),
+        )
+
+        team = Team.objects.create(
+            name="test team",
+            season=season
+        )
+
+        player = User.objects.create(
+            first_name="test",
+            last_name="user2",
+            password="abcd1234"
+        )
+        team.players.add(player, self.user)
+
+        factory = APIRequestFactory()
+        request = factory.get(reverse("api-stats-index"), format="json")
+
+        force_authenticate(request, self.user)
+
+        response = api_views.stats.StatsIndex.as_view()(request)
+        stats = response.data
+        self.assertEquals(stats['league_count'], 1)
+        self.assertEquals(stats['season_count'], 1)
+        self.assertEquals(stats['team_count'], 1)
+        self.assertEquals(stats['player_count'], 2)
+        #TODO rosters
