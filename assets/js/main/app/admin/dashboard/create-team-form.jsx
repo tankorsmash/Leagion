f93af1e44bbff99032d500704e8f4b7b6d59e087
tankorsmash <tankorsmash@gmail.com>
2017-05-13 23:47:46 -0400

    add all league names to TeamCreateForm
    still need to filter down the as-of-yet-implemented seasons input though

--- assets/js/main/app/admin/dashboard/create-team-form.jsx
+++ assets/js/main/app/admin/dashboard/create-team-form.jsx
@@ -2,6 +2,40 @@ import {
     Form, FormGroup, Input, Dropdown, Label,
 } from 'reactstrap';
 
+import {DatasetView} from 'components/dataset_view';
+
+class LeagueSelectInput extends DatasetView {
+    get datasetStateAttr() {
+        return "leagues";
+    }
+
+    get datasetViewName() {
+        return "api-league-list";
+    }
+
+    render() {
+        let options = [];
+        if (this.getIsLoaded() == false) {
+            options.push(<option key={0}> Loading Leagues</option>);
+        } else {
+            this.state.leagues.map((league) => {
+                options.push(<option value={league.id} key={league.id}> {league.name}</option>);
+            });
+        }
+        return (
+            <Input
+                type="select"
+                onChange={this.props.handleInputChange}
+                value={this.props.leagueId}
+                name="leagueId"
+                id="leagueId" >
+                { options }
+            </Input>
+        );
+    }
+}
+
+
 export default class TeamCreateForm extends React.Component {
     render() {
         let formData = this.props.formData;
@@ -19,14 +53,10 @@ export default class TeamCreateForm extends React.Component {
                 </FormGroup>
                 <FormGroup>
                     <Label for="name">League:</Label>
-                    <Input
-                        type="select"
-                        onChange={this.props.handleInputChange}
+                    <LeagueSelectInput
+                        handleInputChange={this.props.handleInputChange}
                         value={formData.leagueId}
-                        name="leagueId"
-                        id="leagueId" >
-                        <option> PLACEHOLDER LEAGUE </option>
-                    </Input>
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label for="name">Season:</Label>
