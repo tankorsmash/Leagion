4a57c30a1eedfbaa0a5def7fe8d9fb456aafb0a6
tankorsmash <tankorsmash@gmail.com>
2017-05-21 19:51:08 -0400

    Add placeholder player detail modal

--- assets/js/main/app/admin/dashboard/players_pane.jsx
+++ assets/js/main/app/admin/dashboard/players_pane.jsx
@@ -1,5 +1,46 @@
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 import DatasetView from 'components/dataset_view';
 import {GeneralTable} from 'main/app/admin/components/table'
+
+class PlayerNameCell extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle(e) {
+        e.preventDefault(); //stop from scrolling to top of page
+
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        const player = this.props.data;
+        return (
+            <td>
+                <a href="#" onClick={this.toggle}> { player.full_name }</a>
+                <Modal isOpen={this.state.isOpen} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </td>
+        );
+    }
+}
 
 export class PlayersPane extends DatasetView {
     get datasetStateAttr() {
@@ -14,6 +55,7 @@ export class PlayersPane extends DatasetView {
         let columns = [{
             id: "full_name",
             title: "Name",
+            component: PlayerNameCell,
         },{
             id: "email",
             title: "Email",
