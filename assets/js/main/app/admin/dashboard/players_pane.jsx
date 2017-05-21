6c15589ac6044817bbe8db68d4f0bfbc267577ca
tankorsmash <tankorsmash@gmail.com>
2017-05-21 19:52:42 -0400

    upgrade reactstrap to 4.6.2 from 4.3.0
    I looked to disable fading and modern versions support it.

--- assets/js/main/app/admin/dashboard/players_pane.jsx
+++ assets/js/main/app/admin/dashboard/players_pane.jsx
@@ -27,7 +27,7 @@ class PlayerNameCell extends React.Component {
         return (
             <td>
                 <a href="#" onClick={this.toggle}> { player.full_name }</a>
-                <Modal isOpen={this.state.isOpen} toggle={this.toggle} className={this.props.className}>
+                <Modal fade={false} isOpen={this.state.isOpen} toggle={this.toggle} className={this.props.className}>
                     <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
                     <ModalBody>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
