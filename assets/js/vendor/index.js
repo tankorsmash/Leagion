deec487e1a4dfa3da194a33a5d5b4fe86f7c3387
Calvin Collins <calvinkcollins@gmail.com>
2017-06-22 22:33:22 -0400

    overidable bootstrap variables

--- assets/js/vendor/index.js
+++ assets/js/vendor/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import '../../style/common/bootstrap.scss';
 import 'toastr/build/toastr.css';
 import 'react-datetime/css/react-datetime.css';
 import 'font-awesome/css/font-awesome.css';
