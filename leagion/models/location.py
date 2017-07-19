2a7abefe7933f6c66b88dfc327929898f1a4effa
tankorsmash <tankorsmash@gmail.com>
2017-07-19 02:47:14 -0400

    Add base for addresses to Locations

--- leagion/models/location.py
+++ leagion/models/location.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+from address.models import AddressField
+
 class Location(models.Model):
     """
-    figure out more details later, like address, weather? etc
+    Using the AddressField purely for the .raw right now.
+    There's a ton more functionality to it but that's all we need
     """
 
     name = models.CharField(max_length=255) #ie Fenway Park
+    address = AddressField(blank=True, null=True)
 
     def __str__(self):
         return "Location: {name}".format(name=self.name)
