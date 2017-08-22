455217bda701ece473b21147cde3101a9561a600
tankorsmash <tankorsmash@gmail.com>
2017-08-22 15:17:26 -0400

    support empty rowdata in tables
    at least, not crashing

--- assets/js/main/app/admin/components/table.js
+++ assets/js/main/app/admin/components/table.js
@@ -240,7 +240,11 @@ export class GeneralTable extends React.Component {
 
     getSortedRowData = () => {
         const sortKey = this.state.sortKey;
-        let rowData = this.props.rowData.slice(); //slice so you copy and dont mutate props.rowData
+
+        let rowData = [];
+        if (typeof(this.props.rowData) !== "undefined") {
+            rowData = this.props.rowData.slice(); //slice so you copy and dont mutate props.rowData
+        };
 
         //fallback to default order
         if (sortKey == undefined || this.props.rowData.length == 0) {
