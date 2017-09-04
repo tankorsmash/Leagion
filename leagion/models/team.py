ab3731ba253d6d6eba78d7a5bb6376fe4d92f8e6
tankorsmash <tankorsmash@gmail.com>
2017-09-04 17:30:59 -0400

    dont encode to string, it's supposed to be unicode now not bytes
    converted some code to functional for fun. tried Ernesto's list comprehension style, not sure if I like it though

--- leagion/models/team.py
+++ leagion/models/team.py
@@ -2,18 +2,19 @@ from django.db import models
 
 from leagion.models import User
 
-#model data
+
 class Team(models.Model):
     name = models.CharField(max_length=255)
-    players = models.ManyToManyField(User, related_name="teams")
     season = models.ForeignKey("Season", related_name="teams")
+
     captains = models.ManyToManyField(User, related_name="captain_of_teams")
+    players = models.ManyToManyField(User, related_name="teams")
 
     def __str__(self):
         return "Team: {name}".format(name=self.name)
 
     def __repr__(self):
-        return "<%s>" % str(self).encode("utf-8")
+        return "<%s>" % str(self)
 
     def get_points(self, wins, draws, losses):
         return wins * 2 + draws
@@ -21,11 +22,18 @@ class Team(models.Model):
     @property
     def win_draw_loss_points(self):
         matches = list(self.home_matches.all()) + list(self.away_matches.all())
-        match_statuses = [match.get_status_for_team(self) for match in matches if match.completed]
+        match_statuses = [
+            match.get_status_for_team(self)
+            for match in matches
+            if match.completed
+        ]
 
-        wins = len([status for status in match_statuses if status == 'W'])
-        draws = len([status for status in match_statuses if status == '-'])
-        losses = len([status for status in match_statuses if status == 'L'])
+        def is_status(value):
+            return lambda to_test: to_test == value
+
+        wins   = len(list(map(is_status("W"), match_statuses)))
+        draws  = len(list(map(is_status("-"), match_statuses)))
+        losses = len(list(map(is_status("L"), match_statuses)))
 
         return {
             'wins': wins,
