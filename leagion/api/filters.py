0f3b28df53e0210e07d8121b5c3785f3d0215e6e
tankorsmash <tankorsmash@gmail.com>
2017-09-29 00:07:56 -0400

    build some one liner helpers for checking permissions and building filters
    this way we can quickly reuse them down the line, since we'll want to be filtering on this stuff a lot

--- leagion/api/filters.py
+++ leagion/api/filters.py
@@ -1,28 +1,45 @@
 from rest_framework import generics, views as drf_views, filters
 
 
+def is_moderator_or_better(user):
+    """is user at least a moderator"""
+    return user.is_staff or user.is_moderator
+
+def build_filter_users_to_commissioner(user):
+    """filter down the users to the leagues the league commissioner can see"""
+    return Q(teams__season__league__league_commissioners=user)
+
+def build_filter_teams_to_commissioner(user):
+    """filter down the teams to the leagues the league commissioner can see"""
+    return Q(season__league__league_commissioners=user)
+
+def build_filter_leagues_to_commissioner(user):
+    """filter down the leagues to the leagues the league commissioner can see"""
+    return Q(league_commissioners=user)
+
+
 class UserFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, user_qs, view):
         user = request.user
-        if user.is_staff or user.is_moderator:
+        if is_moderator_or_better(user):
             return user_qs
 
-        return user_qs.filter(teams__season__league__league_commissioners=user)
+        return user_qs.filter(build_filter_users_to_commissioner(user))
 
 
 class TeamFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, team_qs, view):
         user = request.user
-        if user.is_staff or user.is_moderator:
+        if is_moderator_or_better(user):
             return team_qs
 
-        return team_qs.filter(season__league__league_commissioners=user)
+        return team_qs.filter(build_filter_teams_to_commissioner(user))
 
 
 class LeagueFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, league_qs, view):
         user = request.user
-        if user.is_staff or user.is_moderator:
+        if is_moderator_or_better(user):
             return league_qs
 
-        return league_qs.filter(league_commissioners=user)
+        return league_qs.filter(build_filter_leagues_to_commissioner(user))
