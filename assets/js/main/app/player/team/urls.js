44de6ed627a374fdf620c95d149b334ede96105e
tankorsmash <tankorsmash@gmail.com>
2017-10-01 23:44:15 -0400

    fix team not loading properly
    the url pattern wasn't associating teamId with anything. seems like the slash was the reason

--- assets/js/main/app/player/team/urls.js
+++ assets/js/main/app/player/team/urls.js
@@ -4,7 +4,7 @@ let index = `${playerUrls.index}/team`;
 module.exports = {
 	index: index,
 	detail: `${index}/:teamId?`,
-    detailWide: `${index}/:teamId?/*`,
+    detailWide: `${index}/:teamId?*`,
     detailMatches: `${index}/:teamId?/matches`,
     detailTeamMembers: `${index}/:teamId?/team-members`,
     detailTeamDetails: `${index}/:teamId?/team-details`,
