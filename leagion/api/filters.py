41d6a5d943b1d1f261d7a1f3849067107d819aeb
tankorsmash <tankorsmash@gmail.com>
2017-10-02 23:07:54 -0400

    add default queryset filters for regular players

--- leagion/api/filters.py
+++ leagion/api/filters.py
@@ -8,29 +8,59 @@ def is_moderator_or_better(user):
     """is user at least a moderator"""
     return user.is_staff or user.is_moderator
 
+"""
+Filter down by league commissioner
+"""
 def build_filter_users_to_commissioner(user):
     """filter down the users to the leagues the league commissioner can see"""
     return Q(teams__season__league__league_commissioners=user)
 
 def build_filter_rosters_to_commissioner(user):
-    """filter down the rosters to the leagues the league commissioner can see"""
+    """filter down the rosters to the rosters the league commissioner can see"""
     return Q(match__season__league__league_commissioners=user)
 
 def build_filter_matches_to_commissioner(user):
-    """filter down the matches to the leagues the league commissioner can see"""
+    """filter down the matches to the matches the league commissioner can see"""
     return Q(season__league__league_commissioners=user)
 
 def build_filter_seasons_to_commissioner(user):
-    """filter down the seasons to the leagues the league commissioner can see"""
+    """filter down the seasons to the seasons the league commissioner can see"""
     return Q(league__league_commissioners=user)
 
 def build_filter_teams_to_commissioner(user):
-    """filter down the teams to the leagues the league commissioner can see"""
+    """filter down the teams to the teams the league commissioner can see"""
     return Q(season__league__league_commissioners=user)
 
 def build_filter_leagues_to_commissioner(user):
     """filter down the leagues to the leagues the league commissioner can see"""
     return Q(league_commissioners=user)
+
+"""
+Filter querysets down by standard player
+"""
+def build_filter_users_to_player(user):
+    """filter down the users to the USERs the player can see"""
+    return Q(id=user.id)
+
+def build_filter_rosters_to_player(user):
+    """filter down the rosters to the player can see"""
+    return Q(players=user)
+
+def build_filter_matches_to_player(user):
+    """filter down the matches to the matches the player can see"""
+    return Q((Q(home_roster__players=user)|Q(away_roster__players=user)))
+
+def build_filter_seasons_to_player(user):
+    """filter down the seasons to the seasons the player can see"""
+    return Q(teams__players=user)
+
+def build_filter_teams_to_player(user):
+    """filter down the teams to the teams the player can see"""
+    return Q(players=user)
+
+def build_filter_leagues_to_player(user):
+    """filter down the leagues to the leagues the player can see"""
+    return Q(seasons__teams__players=user)
 
 
 """
@@ -49,8 +79,12 @@ COMMISSIONER_FILTERS = {
     User: lambda u: build_filter_users_to_commissioner(u),
 }
 
-
 def filter_queryset_to_commissioner(user, queryset):
+    """
+    filters a queryset down to whatever the league commissioner can see
+
+    NOTE if they're moderator or better, it returns everything
+    """
     if is_moderator_or_better(user):
         return queryset
 
@@ -60,6 +94,31 @@ def filter_queryset_to_commissioner(user, queryset):
 
     return queryset.filter(qs_filter)
 
+PLAYER_FILTERS = {
+    League: lambda u: build_filter_leagues_to_player(u),
+    Season: lambda u: build_filter_seasons_to_player(u),
+    Roster: lambda u: build_filter_rosters_to_player(u),
+    Match: lambda u: build_filter_matches_to_player(u),
+    Team: lambda u: build_filter_teams_to_player(u),
+    User: lambda u: build_filter_users_to_player(u),
+}
+
+def filter_queryset_to_standard_user(user, queryset):
+    """
+    filters a queryset down to whatever the standard player can see
+
+    NOTE if they're moderator or better, it returns everything
+    """
+    if is_moderator_or_better(user):
+        return queryset
+
+    qs_filter = PLAYER_FILTERS.get(queryset.model)(user)
+    if qs_filter is None:
+        raise Exception("{} model is not in list of filters for filtering down to a regular player".format(queryset.model))
+
+    return queryset.filter(qs_filter)
+
+
 def filter_request_queryset_generic(request, queryset, view):
     """
     filters a queryset down to what the user can see, depending on request method
@@ -67,14 +126,20 @@ def filter_request_queryset_generic(request, queryset, view):
     admins/mods/staff see everything
     commissioners can read and write their own leagues
 
-    TODO: standard user/player permissions
+    standard user/player can only read, nothing else
     """
     user = request.user
 
     if is_moderator_or_better(user):
         return queryset
 
-    return filter_queryset_to_commissioner(user, queryset)
+    if user.is_commissioner:
+        return filter_queryset_to_commissioner(user, queryset)
+
+    if request.method == "GET":
+        return filter_queryset_to_standard_user(user, queryset)
+    else:
+        return queryset.none()
 
 
 class UserFilterBackend(filters.BaseFilterBackend):
