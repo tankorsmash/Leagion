d155c84ac040aebb0bef966b9641e3c4807e96ad
tankorsmash <tankorsmash@gmail.com>
2017-12-08 00:44:32 -0500

    clean up data exporter test
    might be overboard again though

--- leagion/tests/test_data_importer.py
+++ leagion/tests/test_data_importer.py
@@ -1,6 +1,8 @@
-import time
 import csv
+import time
 import datetime
+
+from functools import partial
 
 from django.test import TestCase, override_settings
 from django.contrib.auth import get_user_model
@@ -22,6 +24,7 @@ class BaseDataExporter(APITestCase, CreatorMixin):
     test the creation of the CSV template for Data Imports
     """
 
+NO_DATA = 0xDEADBEEF
 
 class TeamExporterTestCase(BaseDataExporter):
     def setUp(self):
@@ -33,12 +36,30 @@ class TeamExporterTestCase(BaseDataExporter):
         for _ in range(5):
             self.teams.append(self.create_team(self.season))
 
+    def assertRowMatch(self, row_data, team, col_id, team_attr, expected_data=NO_DATA):
+        if expected_data == NO_DATA:
+            expected_data = getattr(team, team_attr)
+
+        self.assertEquals(
+            getattr(row_data, col_id).data,
+            expected_data,
+        )
+
     def test_export_teams_in_league_template(self):
         #assert all teams' ids show up
         for row_data in self.team_gen.next_row():
             team_id = row_data.team_id
             team = Team.objects.get(id=row_data.team_id.data)
-            self.assertEquals(row_data.team_id.data, team.id)
+            assert_row = partial(self.assertRowMatch, row_data, team)
+
+            assert_row("team_id", "id")
+            assert_row("team_name", "name")
+
+            team_captains_names = ", ".join(cap.full_name for cap in team.captains.all())
+            assert_row("team_captain_full_name", None, team_captains_names)
+            team_captains_emails = ", ".join(cap.email for cap in team.captains.all())
+            assert_row("team_captain_email", None, team_captains_names)
+
 
 
 class LocationsExporterTestCase(BaseDataExporter):
